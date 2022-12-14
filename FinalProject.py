f450a1ef64108ebebb77995949d602f8842e3d02
Ludus92 <50162502+Ludus92@users.noreply.github.com>
2022-12-14 16:10:10 -0500

    Add files via upload

--- FinalProject.py
+++ FinalProject.py
@@ -29,51 +29,40 @@ test2 = ImageTk.PhotoImage(image2)
 
 #setting 1st image in label l3
 l3=tk.Label(root,image=test)
-l3.place(x=100,y=10)#position the label
+l3.place(x=100,y=10)
 
 #setting 2nd image in label l4
-
 l4=tk.Label(root,image=test2)
-l4.place(x=450,y=10)#position the label
+l4.place(x=450,y=10)
 
-#displaying a menu for pizza using a label with font and size of text
-
+#Creating respective labels and buttons for each game.
 l5=tk.Label(root,text="Dropship Remastered",font=("Arial", 15))
 l5.place(x=100,y=200)
-
-#displaying a menu for hot dog
 
 l6=tk.Label(root,text="Mercs: Remastered",font=("Arial", 15))
 l6.place(x=400,y=200)
 
-
 l1=tk.Button(root,text="Click to Buy",font=("Arial", 15))
 l1.place(x=150,y=280)
-
 
 l2=tk.Button(root,text="Click to Buy",font=("Arial", 15))
 l2.place(x=400,y=280)
 
-l7=Button(root,text="Shopping Cart",font=("Arial", 15))
+#Creation of the window and widgets.
+def create_top():
+    top=Toplevel(root)
+    top.geometry("600x300")
+    top.title("Shopping Cart")
+    Label(top, text="Sorry the cart is currently unavailable.", font='Arial 15 bold').pack()
+    button=tk.Button(top, text="Close", command=top.destroy)
+    button.pack()
+
+#Upon clicking the shopping cart button a new window pops up.
+l7=tk.Button(root,text="Shopping Cart",font=("Arial", 15), command=create_top)
 l7.place(x=250,y=400)
 
-l7.bind("<Button>",
-         lambda e: NewWindow(root))
-
-priceOfDrop=l1*24.99
-priceOfMerc=l2*34.99
-
-class NewWindow(Toplevel):
-     
-    def __init__(self, master = None):
-        super().__init__(master = master)
-        self.title("Shopping Cart")
-        self.geometry("600x300")
-        label = Label(self, text ="Items in cart:")
-        label.pack()
-
-    def itemcart():
-
+l9=tk.Button(root,text="Exit",font=("Arial", 15), command=root.destroy)
+l9.place(x=50,y=400)
 
 #starting the main window
 root.mainloop()
